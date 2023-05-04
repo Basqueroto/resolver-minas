b9408ccb0d5b9c7660ae0d4e213592d31480cdf5
basqueoro <basqueroto.felipe125@gmail.com>
2023-05-04 13:32:22 -0300

    ifs para ver as cores

--- mina.py
+++ mina.py
@@ -20,86 +20,91 @@ compY = 540
 numColumns = 10
 numRows = 8
 
-
 def foto():
     p.screenshot("boardInit.png", region=(startX, startY, compX, compY))
 
 def compare ():
-    principal = cv.imread("quad.png")
-    rose1 = cv.imread("img\image60.png")
-
-    (b, g, r) = rose1[5, 5]
-    print ("Cor do pixel em (5, 5) - Vermelho: %d, Verde: %d, Azul: %d" % (r, g, b))
-
+    rose1 = cv.imread("quad.png")
+    (bF, gF, rF) = rose1[5, 5]
     (b, g, r) = rose1[34, 34]
     print ("Cor do pixel em (34, 34) - Vermelho: %d, Verde: %d, Azul: %d" % (r, g, b))
 
+    if g < 200:
+        if r > 210 and b > 130:
+            return "fundo vermelho"
+        elif  g > 170 and r < 200:
+            return "verde"
+        elif g > 110:
+            return "azul"
+        elif g > 60:
+            return "red"
+    else: 
+        return "fundo verde"    
 
-  
-compare() 
 
 # def encontrar ():        
 
-# def percorrer (): 
-#     quadX = 67
-#     quadY = 67
-#     quadStartX = startX
-#     quadStartY = startY
-#     contador = 0   
-#     for i in range (numRows):
-#         for j in range (numColumns):
-#             image = p.screenshot("quad.png", region=(quadStartX, quadStartY, quadX, quadY))
-#             contador+=1
-#             image.save(r'C:\Users\Aluno\Desktop\resolver-minas\img\image{}.png'.format(contador))
 
+def percorrer (): 
+    quadX = 67
+    quadY = 67
+    quadStartX = startX
+    quadStartY = startY
+    contador = 0   
+    for i in range (numRows):
+        for j in range (numColumns):
+            image = p.screenshot("quad.png", region=(quadStartX, quadStartY, quadX, quadY))
+            contador+=1
+            image.save(r'C:\Users\Aluno\Desktop\resolver-minas\img\image{}.png'.format(contador))
+            print(compare())
+            print(contador)
+            quadStartX += quadX
 
+        quadStartX = startX
+        quadStartY += quadY 
+    p.alert("o loop terminou") 
 
-#             quadStartX += quadX
+#----------------------ABRIR o jogo-------------------------------- 
 
-#         quadStartX = startX
-#         quadStartY += quadY 
-#     p.alert("o loop terminou")       
-# #----------------------ABRIR o jogo-------------------------------- 
+p.alert("O código vai começar. Não utilize nada do computador até o código finalizar!")
+p.PAUSE = 0.5
 
-# p.alert("O código vai começar. Não utilize nada do computador até o código finalizar!")
-# p.PAUSE = 0.5
+p.moveTo(1,1)
 
-# p.moveTo(1,1)
+# abrir google
+p.press('winleft')
+p.write('google')
+p.press('enter')
 
-# # abrir google
-# p.press('winleft')
-# p.write('google')
-# p.press('enter')
+time.sleep(2)
+# abrir campo minado
+p.write('campo minado')
+p.press('enter')
+p.press('f11')
+p.moveTo(567,600) #chorme
+# p.moveTo(381,358) #opera
+time.sleep(2)
+p.click()
 
-# time.sleep(2)
-# # abrir campo minado
-# p.write('campo minado')
-# p.press('enter')
-# p.press('f11')
-# p.moveTo(567,600) #chorme
-# # p.moveTo(381,358) #opera
-# time.sleep(2)
+p.moveTo(595,240) 
+p.click()
+p.moveTo(595,265)
+p.click()
+p.moveTo(645,335)
+p.click()
+
+# p.moveTo(437,298) opera
+# p.click()
+# p.moveTo(432,331)
+# p.click()
+# p.moveTo(434,382)
 # p.click()
 
-# p.moveTo(595,240) 
-# p.click()
-# p.moveTo(595,265)
-# p.click()
-# p.moveTo(645,335)
-# p.click()
+time.sleep(1)
+foto()
 
-# # p.moveTo(437,298) opera
-# # p.click()
-# # p.moveTo(432,331)
-# # p.click()
-# # p.moveTo(434,382)
-# # p.click()
-
-# time.sleep(1)
-# foto()
-
-# time.sleep(2)
-# percorrer()
+time.sleep(2)
+percorrer()
 
 
 
