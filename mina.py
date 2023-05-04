f3da5aad1185811dc689df5c1a8cf329414d3336
basqueoro <basqueroto.felipe125@gmail.com>
2023-05-04 11:00:17 -0300

    funçaõ de ver cores criada

--- mina.py
+++ mina.py
@@ -8,10 +8,13 @@ import cv2 as cv
 # inferior
 # x = 863
 # y = 720
-startX = 415
-startY = 361
-compX = 448
-compY = 361
+startX = 623
+startY = 313
+# compX = 448 google
+# compY = 361
+
+compX = 674
+compY = 540
 
 # dimenções do campo
 numColumns = 10
@@ -22,49 +25,38 @@ def foto():
     p.screenshot("boardInit.png", region=(startX, startY, compX, compY))
 
 def compare ():
-    principal = cv.imread("1.png")
-    rose1 = cv.imread("1.png")
+    principal = cv.imread("quad.png")
+    rose1 = cv.imread("img\image60.png")
 
-    principal = cv.resize(principal, (44, 44))
-    rose1 = cv.resize(rose1, (44, 44))
+    (b, g, r) = rose1[5, 5]
+    print ("Cor do pixel em (5, 5) - Vermelho: %d, Verde: %d, Azul: %d" % (r, g, b))
 
-    orb = cv.ORB_create()
-
-    kp_a, desc_a = orb.detectAndCompute(principal, None)
-    kp_b, desc_b = orb.detectAndCompute(rose1, None)
-
-    bf = cv.BFMatcher(cv.NORM_HAMMING, crossCheck=True)
-
-    matches = bf.match(desc_a, desc_b)
-
-    similar_regions = [i for i in matches if i.distance < 50]
-    if len(matches) == 0:
-        return 0
-    return len(similar_regions) / len(matches)
+    (b, g, r) = rose1[34, 34]
+    print ("Cor do pixel em (34, 34) - Vermelho: %d, Verde: %d, Azul: %d" % (r, g, b))
 
 
-print("O Percentual de Similaridade Utilizando o ORB é: ", compare())    
- 
+  
+compare() 
 
 # def encontrar ():        
 
 # def percorrer (): 
-#     quadX = 44.8
-#     quadY = 44.8
-#     quadStartX = 415
-#     quadStartY = 361
+#     quadX = 67
+#     quadY = 67
+#     quadStartX = startX
+#     quadStartY = startY
 #     contador = 0   
 #     for i in range (numRows):
 #         for j in range (numColumns):
 #             image = p.screenshot("quad.png", region=(quadStartX, quadStartY, quadX, quadY))
 #             contador+=1
-#             image.save(r'C:\Users\Felipe Basqueroto\OneDrive\Área de Trabalho\py\resolver-minas\img\image{}.png'.format(contador))
+#             image.save(r'C:\Users\Aluno\Desktop\resolver-minas\img\image{}.png'.format(contador))
 
 
 
 #             quadStartX += quadX
 
-#         quadStartX = 415
+#         quadStartX = startX
 #         quadStartY += quadY 
 #     p.alert("o loop terminou")       
 # #----------------------ABRIR o jogo-------------------------------- 
@@ -76,7 +68,7 @@ print("O Percentual de Similaridade Utilizando o ORB é: ", compare())
 
 # # abrir google
 # p.press('winleft')
-# p.write('opera')
+# p.write('google')
 # p.press('enter')
 
 # time.sleep(2)
@@ -84,24 +76,24 @@ print("O Percentual de Similaridade Utilizando o ORB é: ", compare())
 # p.write('campo minado')
 # p.press('enter')
 # p.press('f11')
-# # p.moveTo(567,600) #chorme
-# p.moveTo(381,358) #opera
+# p.moveTo(567,600) #chorme
+# # p.moveTo(381,358) #opera
 # time.sleep(2)
 # p.click()
 
-# # # p.moveTo(595,240) chorme----------------
-# # p.click()
-# # p.moveTo(595,265)
-# # p.click()
-# # p.moveTo(645,335)
-# # p.click()
+# p.moveTo(595,240) 
+# p.click()
+# p.moveTo(595,265)
+# p.click()
+# p.moveTo(645,335)
+# p.click()
 
-# p.moveTo(437,298)
-# p.click()
-# p.moveTo(432,331)
-# p.click()
-# p.moveTo(434,382)
-# p.click()
+# # p.moveTo(437,298) opera
+# # p.click()
+# # p.moveTo(432,331)
+# # p.click()
+# # p.moveTo(434,382)
+# # p.click()
 
 # time.sleep(1)
 # foto()
