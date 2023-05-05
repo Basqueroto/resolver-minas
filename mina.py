2a42775039fdc9de04d4461c6a50dcba8d7031f8
basqueoro <basqueroto.felipe125@gmail.com>
2023-05-05 17:21:51 -0300

    desenvolvimento da procura

--- mina.py
+++ mina.py
@@ -3,24 +3,24 @@ import time
 import cv2 as cv
 
 
-# startX = 623 #google 
-# startY = 313
+startX = 623 #google 
+startY = 313
 
-startX = 415 #opera
-startY = 361
+# startX = 415 #opera
+# startY = 361
 
-compX = 448 #opera
-compY = 361
+# compX = 448 #opera
+# compY = 361
 
-# compX = 674 #google
-# compY = 540
+compX = 674 #google
+compY = 540
 
 # dimenções do campo
 numColumns = 10
 numRows = 8
 
-quadX = 44
-quadY = 44
+quadX = 67
+quadY = 67
 quadStartX = 0
 quadStartY = 0
 
@@ -30,7 +30,7 @@ def foto():
 def compare ():
     rose1 = cv.imread("quad.png")
     # (bF, gF, rF) = rose1[10, 10]
-    (b, g, r) = rose1[21, 21]
+    (b, g, r) = rose1[34, 34]
     # print ("Cor do pixel em (34, 34) - Vermelho: %d, Verde: %d, Azul: %d" % (r, g, b))
 
     if g < 200:
@@ -50,60 +50,59 @@ def compare ():
         print("fundo verde")
         return 6    
 
-
 def encontrar (indice,x,y):
     position = [[x - quadX, y], [x - quadX, y - quadY], [x, y - quadY], [x + quadX, y - quadY], [x + quadX, y], [x + quadX, y + quadY], [x, y + quadY], [x - quadX, y + quadY]]
     #verde = 0 bandeira = 1 aberto = 2
     baixo = [0,0,0,0,0,0,0,0]
     vazio = 0
-    bombas = indice        
-    if indice != 6:  
-        print(indice, "a função foi iniciada")
-        for i in range(0, len(position)):
-            b = p.screenshot("redor.png", region=(position[i][0], position[i][1], quadX, quadY))
-            b.save(r'C:\Users\Felipe Basqueroto\OneDrive\Área de Trabalho\minas2\resolver-minas\imgachar\image{}.png'.format(i))
-            b = cv.imread("redor.png")
-            (bF, gF, rF) = b[5, 5]
-            (b, g, r) = b[22, 22]
-            print ("Vermelho: %d, Verde: %d, Azul: %d" % (rF, gF, bF))
-            if rF > 200 and bF > 120:
-                print("fundo vermelho achado")
-                vazio += 1
-                baixo[i] = 2
-            elif gF > 199 and g < 90 and r > 190:
-                print("bandeira achada")
-                vazio += 1
-                bombas -= 1
-                baixo[i] = 1
+    bombas = int(indice)        
+    print(indice, "a função foi iniciada")
+    for i in range(0, len(position)):
+        b = p.screenshot("redor.png", region=(position[i][0], position[i][1], quadX, quadY))
+        b.save(r'C:\Users\Aluno\Desktop\resolver-minas\imgachar\image{}.png'.format(i))
+        b = cv.imread("redor.png")
+        (bF, gF, rF) = b[5, 5]
+        (b, g, r) = b[34, 34]
+        print ("Vermelho: %d, Verde: %d, Azul: %d" % (rF, gF, bF))
+        if rF > 210:
+            print("fundo vermelho achado")
+            vazio += 1
+            baixo[i] = 2
+        elif gF > 199 and g < 90 and r > 190:
+            print("bandeira achada")
+            vazio += 1
+            bombas = bombas -  1
+            baixo[i] = 1
         
-        print(vazio)
-        print(bombas)
-        print(baixo)
-        if bombas <= 0: 
-            for i in range(0, len(baixo)):
-                if baixo[i] == 0:
-                    p.moveTo(position[i][0] + quadX / 2,position[i][1] + quadY / 2)
-                    p.click()
-        if 8 - vazio == bombas:
-            print("----------------dentro do if das bombas----------------")
-            for i in range(0, len(baixo)):
-                if baixo[i] == 0:
-                    p.moveTo(position[i][0] + quadX / 2,position[i][1] + quadY / 2)
-                    p.rightClick()
-    return                
+    print(vazio)
+    print(bombas)
+    print(baixo)
+    if bombas <= 0: 
+        print("-------------------achou um lugar para clicar--------------------------")
+        for i in range(0, len(baixo)):
+            if baixo[i] == 0:
+                p.moveTo(position[i][0] + quadX / 2 ,position[i][1] + quadY / 2)
+                p.click()
+    if 8 - vazio == bombas:
+        print("----------------dentro do if das bombas----------------")
+        for i in range(0, len(baixo)):
+            if baixo[i] == 0:
+                p.moveTo(position[i][0] + quadX / 2,position[i][1] + quadY / 2)
+                p.rightClick()               
 
 def percorrer (): 
     quadStartX = startX
     quadStartY = startY
     contador = 0   
-    for i in range (numRows):
-        for j in range (numColumns):
+    for i in range (0, numRows):
+        for j in range (0, numColumns):
             image = p.screenshot("quad.png", region=(quadStartX, quadStartY, quadX, quadY))
             contador+=1
-            image.save(r'C:\Users\Felipe Basqueroto\OneDrive\Área de Trabalho\minas2\resolver-minas\img\image{}.png'.format(contador))
+            image.save(r'C:\Users\Aluno\Desktop\resolver-minas\img\image{}.png'.format(contador))
             print(compare())
             print(contador)
-            encontrar(compare(),quadStartX, quadStartY)  
+            if (compare() != 6):
+                encontrar(compare(),quadStartX, quadStartY)  
             quadStartX += quadX
         quadStartX = startX
         quadStartY += quadY 
@@ -118,7 +117,7 @@ p.moveTo(1,1)
 
 # abrir google
 p.press('winleft')
-p.write('opera')
+p.write('google')
 p.press('enter')
 
 time.sleep(2)
@@ -126,24 +125,24 @@ time.sleep(2)
 p.write('campo minado')
 p.press('enter')
 p.press('f11')
-# p.moveTo(567,600) #chorme
-p.moveTo(381,358) #opera
+p.moveTo(567,600) #chorme
+# p.moveTo(381,358) #opera
 time.sleep(2)
 p.click()
 
-# p.moveTo(595,240) #google
-# p.click()
-# p.moveTo(595,265)
-# p.click()
-# p.moveTo(645,335)
-# p.click()
+p.moveTo(595,240) #google
+p.click()
+p.moveTo(595,265)
+p.click()
+p.moveTo(645,335)
+p.click()
 
-p.moveTo(437,298) #opera
-p.click()
-p.moveTo(432,331)
-p.click()
-p.moveTo(434,382)
-p.click()
+# p.moveTo(437,298) #opera
+# p.click()
+# p.moveTo(432,331)
+# p.click()
+# p.moveTo(434,382)
+# p.click()
 
 time.sleep(1)
 foto()
@@ -153,5 +152,5 @@ q = 0
 while True:
     q = q + 1
     percorrer()
-    if (q > 10):
+    if (q > 50):
         break
