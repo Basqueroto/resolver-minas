f95c2475baa04048f261b4764c7697f9070c047a
basqueoro <basqueroto.felipe125@gmail.com>
2023-05-26 17:01:06 -0300

    resolver muitos problemas n quebra mais

--- cor.py
+++ cor.py
@@ -7,15 +7,19 @@ import cv2 as cv
 #vermelho: b=47 v=47 verme=211
 
 y = 0
-rose1 = cv.imread("img/image33.png")
+rose1 = cv.imread("img/image34.png")
 # (b, g, r) = rose1[34, 34] #centro1
 # (b2, g2, r2) = rose1[50, 40] #centro2
 # (b3, g3, r3) = rose1[38, 40] #centro3
-
+# (b, g, r) = rose1[34, 34]
 # (b2, g2, r2) = rose1[10, 10]
-# print('pixel do meio azul',b,'verde', g,'vermelho', r)
 # print('pixel na ponta azul',b2,'verde', g2,'vermelho', r2)
-# print('pixel na ponta azul',b3,'verde', g3,'vermelho', r3)
+# print('pixel do meio azul',b,'verde', g,'vermelho', r)
+
+# if r2 > 210:
+#     print("fundo vermelho achado")
+# elif r > 190:
+#     print("bandeira achada")
 
 def color (image):
     medB = 0
@@ -36,16 +40,24 @@ def color (image):
     print(medB, medV, medVer)
     return medB, medV, medVer
 
-(b,g,r) = color("img/image33.png")
+# (b,g,r) = color("img/image52.png")
 
-if g < 200:
-        if b > 140 and g > 170 and r > 210:
-            print ("fundo vermelho")
-        elif  g > 170 and r < 200:
-            print ("verde")
-        elif g > 110:
-            print("azul")
-        elif g > 60:
-            print("red")
-else: 
-    print("fundo verde")
+# if g < 205:
+#         if b > 140 and g > 170 and r > 210:
+#             print ("fundo vermelho")
+#         elif  b < 160 and g > 150 and r < 200:
+#             print ("verde")
+#         elif b >= 160 and g > 150 and r < 200:
+#             print("azul")
+#         elif b < 160 and g > 60 and r >= 200:
+#             print("red")
+# else: 
+#     print("fundo verde")
+
+(bF, gF, rF) = rose1[10, 10]
+print(rF)
+(b,g,r) = color('img/image34.png')
+if rF > 200:
+    print("fundo vermelho achado")
+elif b < 160 and g > 60 and r >= 190:
+    print("bandeira achada")
--- mina.py
+++ mina.py
@@ -26,31 +26,48 @@ quadY = 67
 quadStartX = 0
 quadStartY = 0
 
+def color (image):
+    medB = 0
+    medV = 0
+    medVer = 0
+    cont = 0
+    img = cv.imread(image)
+    for i in range(21, 57):
+        for j in range(23, 45):
+            (b, g, r) = img[i, j]        
+            medB += b
+            medV += g
+            medVer += r
+            cont += 1
+    medB = medB / cont
+    medV = medV / cont
+    medVer = medVer / cont
+    # print(medB, medV, medVer)
+    return medB, medV, medVer
+
 def foto():
     p.screenshot("boardInit.png", region=(startX, startY, compX, compY))
 
 def compare ():
-    rose1 = cv.imread("quad.png")
-    # (bF, gF, rF) = rose1[10, 10]
-    (b, g, r) = rose1[34, 34]
-    # print ("Cor do pixel em (34, 34) - Vermelho: %d, Verde: %d, Azul: %d" % (r, g, b))
-
-    if g < 200:
-        if r > 210 and b > 130:
+    (b,g,r) = color("quad.png")
+    if g < 205:
+        if b > 140 and g > 170 and r > 210:
             print ("fundo vermelho")
             return 6
-        elif  g > 170 and r < 200:
+        elif  b < 160 and g > 150 and r < 200:
             print ("verde")
             return 2
-        elif g > 110:
+        elif b >= 160 and g > 150 and r < 200:
             print("azul")
             return 1
-        elif g > 60:
+        elif b < 160 and g > 60 and r >= 200:
             print("red")
             return 3
+        else:
+            return 6
     else: 
         print("fundo verde")
-        return 6    
+        return 6  
 
 def encontrar (indice,x,y):
     position = [[x - quadX, y], [x - quadX, y - quadY], [x, y - quadY], [x + quadX, y - quadY], [x + quadX, y], [x + quadX, y + quadY], [x, y + quadY], [x - quadX, y + quadY]]
@@ -63,39 +80,58 @@ def encontrar (indice,x,y):
         b = p.screenshot("redor.png", region=(position[i][0], position[i][1], quadX, quadY))
         b.save(r'C:\Users\Aluno\Desktop\resolver-minas\imgachar\image{}.png'.format(i))
         b = cv.imread("redor.png")
-        (bF, gF, rF) = b[5, 5]
-        (b, g, r) = b[34, 34]
-        # print ("Vermelho: %d, Verde: %d, Azul: %d" % (rF, gF, bF))
+        (bF, gF, rF) = b[10, 10]
+        (b,g,r) = color("redor.png")
         if rF > 210:
             print("fundo vermelho achado")
             vazio += 1
             baixo[i] = 2
-        elif r > 190:
+        elif b < 160 and g > 60 and r >= 190:
             print("bandeira achada")
             vazio += 1
             bombas -= 1
             baixo[i] = 1
 
     if x + quadX > fimX:
+        if baixo[3] != 2 or baixo[3] != 2:
+            vazio += 1
+        if baixo[4] != 2 or baixo[4] != 2:
+            vazio += 1
+        if baixo[5] != 2 or baixo[5] != 2:
+            vazio += 1            
         baixo[3] = 2
         baixo[4] = 2
         baixo[5] = 2
-        vazio += 3
     if x - quadX < fimX:
+        if baixo[0] != 2 or baixo[0] != 2:
+            vazio += 1
+        if baixo[1] != 2 or baixo[1] != 2:
+            vazio += 1
+        if baixo[7] != 2 or baixo[7] != 2:
+            vazio += 1 
         baixo[0] = 2
         baixo[1] = 2
         baixo[7] = 2
-        vazio += 3
     if y + quadY > fimY:
+        if baixo[5] != 2 or baixo[5] != 2:
+            vazio += 1
+        if baixo[6] != 2 or baixo[6] != 2:
+            vazio += 1
+        if baixo[7] != 2 or baixo[7] != 2:
+            vazio += 1 
         baixo[5] = 2
         baixo[6] = 2
         baixo[7] = 2
-        vazio += 3
     if y - quadY < fimY:
+        if baixo[1] != 2 or baixo[1] != 2:
+            vazio += 1
+        if baixo[2] != 2 or baixo[2] != 2:
+            vazio += 1
+        if baixo[3] != 2 or baixo[3] != 2:
+            vazio += 1 
         baixo[1] = 2
         baixo[2] = 2
         baixo[3] = 2
-        vazio += 3
                    
     print('estão vazioes esses espaços ',vazio)
     print('bombas restantes', bombas)
@@ -123,12 +159,12 @@ def percorrer ():
             contador+=1
             image.save(r'C:\Users\Aluno\Desktop\resolver-minas\img\image{}.png'.format(contador))
             print(contador)
-            # if (compare() != 6):
-            #     encontrar(compare(),quadStartX, quadStartY)  
+            t = compare()
+            if (t != 6):
+                encontrar(t,quadStartX, quadStartY)  
             quadStartX += quadX
         quadStartX = startX
         quadStartY += quadY 
-    p.alert("o loop terminou") 
 
 #----------------------ABRIR o jogo-------------------------------- 
 
@@ -168,13 +204,15 @@ p.click()
 
 time.sleep(1)
 foto()
+p.PAUSE = 0.2
 
 time.sleep(2)
 q = 0
-# while True:
-#     q = q + 1
-#     percorrer()
-#     if (q > 1):
-#         break
-# time.sleep(2)
-percorrer()
+while True:
+    q = q + 1
+    percorrer()
+    if (q > 10):
+        break
+p.alert("o loop terminou")     
+time.sleep(2)
+# percorrer()
